7d56aa580fb45294d90a89d1f0c918abbc3cecde
Daniel Wallace <danielwallace@gtmanfred.com>
2022-06-23 15:42:36 -0400

    transpile vuetify

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -7,6 +7,7 @@ import vuetify from "@vuetify/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
+  transpileDependencies: ['vuetify'],
   base: process.env.NODE_ENV === "production" ? "/ui/" : "/",
   resolve: {
     alias: {
